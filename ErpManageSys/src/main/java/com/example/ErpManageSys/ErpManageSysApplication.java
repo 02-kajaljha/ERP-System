cdb8fac6e4cfc01150f8c57410dc5b4202f36010
kajal jha <kajalanjujha@gmail.com>
2025-04-29 01:03:41 +0530

    Initial commit - Student ERP System

---
+++ ErpManageSys/src/main/java/com/example/ErpManageSys/ErpManageSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.ErpManageSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpManageSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpManageSysApplication.class, args);
+	}
+
+}
